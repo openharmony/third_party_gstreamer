73c97d6575825d757138b849a03322a2d50e24fb
xxb-wzy <xuxiaobo5@huawei.com>
2022-08-15 21:22:07 +0800

    Signed-off-by: xxb-wzy <xuxiaobo5@huawei.com> Change-Id: Ib74aa55fb030f1af6d59e68a77f4f32a9f2f05f6

--- gstplugins_base/gst-libs/gst/tag/id3v2frames.c
+++ gstplugins_base/gst-libs/gst/tag/id3v2frames.c
@@ -110,6 +110,10 @@ id3v2_parse_frame (ID3TagsWorking * work)
   if (work->frame_flags & (ID3V2_FRAME_FORMAT_COMPRESSION |
           ID3V2_FRAME_FORMAT_DATA_LENGTH_INDICATOR)) {
 #ifdef OHOS_OPT_CVE
+    /*
+     * ohos.opt.cve.0002
+     * CVE-2021-3522 : https://gstreamer.freedesktop.org/security/sa-2021-0001.html
+     */
     if (frame_data_size <= 4)
 #else
     if (work->hdr.frame_data_size <= 4)
